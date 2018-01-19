33040cdc56d92898ecfb60d0d76f1d10a2c1e074
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-01-19 19:44:42 +0900

    Update run.js

--- run.js
+++ run.js
@@ -1,3 +1,7 @@
 function run(el){
-console.log(el,this)
+ var data= el.src.split('?q=');
+ var baseurl =data[0].slice(0, data[0].lastIndexOf('/')+1 );
+ var ary = data[1].trim().split('|');
+ console.log(ary)
+ 
 }
