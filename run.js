d1830bf68d0e7b47ebe4d820840c22a2ac2a8281
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-01-19 20:30:17 +0900

    Update run.js

--- run.js
+++ run.js
@@ -3,17 +3,18 @@ function run(el){
  var baseurl =data[0].slice(0, data[0].lastIndexOf('/')+1 );
  var t =el.parentNode; 
  var ary = data[1].trim().split('|');
- 
  ary.forEach((d)=>{
  var url=d.split('?')[0];
  if(~url.indexOf('.js')){
   var el= document.createElement('script');
-  t.appendChild(el);
+  //t.appendChild(el);
+  t.insertBefore( el , t.firstElementChild); 
   el.src=baseurl+d;
  }else if(~url.indexOf('.css')){
   var el = document.createElement('link');
   el.setAttribute('rel','stylesheet');
-  t.appendChild(el);
+  //t.appendChild(el);
+  t.insertBefore( el , t.firstElementChild); 
   el.setAttribute('href',baseurl+d);  
  }
 })
