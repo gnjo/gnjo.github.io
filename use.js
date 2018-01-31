97c554d4c236da75441ee06d78a5117f336ebe27
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-01-31 19:58:44 +0900

    Update use.js

--- use.js
+++ use.js
@@ -163,3 +163,24 @@ fn.toBlob =function(base64) {
 //data:image/png;base64,... 
 }
 
+fn.copy=function(textVal){
+  // テキストエリアを用意する
+  var copyFrom = document.createElement("textarea");
+  // テキストエリアへ値をセット
+  copyFrom.textContent = textVal;
+ 
+  // bodyタグの要素を取得
+  var bodyElm = document.getElementsByTagName("body")[0];
+  // 子要素にテキストエリアを配置
+  bodyElm.appendChild(copyFrom);
+ 
+  // テキストエリアの値を選択
+  copyFrom.select();
+  // コピーコマンド発行
+  var retVal = document.execCommand('copy');
+  // 追加テキストエリアを削除
+  bodyElm.removeChild(copyFrom);
+  // 処理結果を返却
+  return retVal;
+}
+
