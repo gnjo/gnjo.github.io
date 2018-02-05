ff3f04b00d4e55f90f51735bbfd1874c680b9df9
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-02-05 17:10:12 +0900

    Create e.js

---
+++ e.js
@@ -0,0 +1,92 @@
+(function(root){
+ 
+ var entry =function(obj){
+ 
+  let e={}
+  ;
+  e.isElement = function(obj){return !!(obj && obj.nodeType === 1)}
+  e.isQuery =function(obj){
+    return !( /^</.test(obj.trim()) )
+}
+  e.elcopy=function(el,flg=true){
+  let obj={}
+  ;
+   for(const d in el){ if( /^on(.*)/.test(d) && el[d]) obj[d]=el[d] }
+   return Object.assign( el.cloneNode(flg) ,obj);
+}
+  ;///
+  e._ ={};
+  e.copy =function(obj){e._['copy'] =obj;return this;}
+  e.on =function(obj){e._['on'] =obj;return this;}
+  e.attr =function(obj){e._['attr'] =obj;return this;}
+  e.data =function(obj){e._['data'] =obj;return this;}
+  e.appendTo =function(obj){e._['appendTo'] =obj;return this;}
+  e.prependTo =function(obj){e._['prependTo'] =obj;return this;}
+  e.siblTo =function(obj){e._['siblTo'] =obj;return this;}
+  e.presiblTo =function(obj){e._['presiblTo'] =obj;return this;}
+  e.effect =function(obj){e._['effect'] =obj;return this;}
+  e.end=function(log){
+   if(log) console.log(log);
+   let o = this._
+   ,el = document.createElement('table')
+   ;
+
+   if( e.isElement(o['e']) ) el=o['e']
+   else if( e.isQuery(o['e'])) el =document.querySelector(o['e'])
+   else {el.innerHTML= o['e']; el= el.firstChild;}
+
+   if(o['copy']) el = e.elcopy(el,o['copy'])
+   if(o['on']) Object.keys(o['on']).forEach((k)=>{ el['on'+k ]= o['on'][k] })
+   if(o['attr']) Object.keys(o['attr']).forEach((k)=>{ el.setAttribute(k,o['attr'][k]) })
+   if(o['data']) Object.keys(o['data']).forEach((k)=>{ el.setAttribute('data-'+k,o['data'][k]) })
+   //
+   if(o['effect']){
+     let p =(d)=>{return isNaN( parseFloat(d))? 0 : parseFloat(d)*1000 }
+     Object.keys(o['effect']).forEach((k)=>{ 
+      el.classList.add(k); 
+      setTimeout(()=>{ el.classList.remove(k) }, p(o['effect'][k]) )   
+     })
+   } 
+   //
+   ;
+   let tar =(obj)=>{return ( e.isElement(obj) )?obj:document.querySelector(obj)}
+   ,fr=(elm)=>{let f=document.createDocumentFragment();f.appendChild(elm);return f}
+   ;
+   if(o['appendTo']){
+    let parent=tar(o['appendTo']);
+    parent.appendChild( fr(el) );
+    return el;
+   }
+   if(o['prependTo']){
+    //parent.insertBefore(el, parent.firstChild);
+    let parent =tar(o['prependTo']);  
+    parent.insertBefore( fr(el) , parent.firstChild);   
+    return el;
+   }
+   if(o['siblTo']){
+    let parent =tar(o['siblTo'])
+     parent.parentNode.insertBefore( fr(el) , parent.nextSibling); 
+     //parent.nextElementSibling().appendChild(el);
+    return el;
+   }
+   if(o['presiblTo']){
+    let parent =tar(o['presiblTo'])
+     parent.parentNode.insertBefore( fr(el) ,parent)   
+     //parent.previousElementSibling().appendChild(el)
+    return el;
+   }
+   //
+   //console.log('el is alone')
+
+   return el;
+  }
+  ;
+  ////
+  e._['e'] = obj
+  ;
+  return e;
+ }
+
+ root.e = entry;
+ 
+})(this)
