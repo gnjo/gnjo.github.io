e4d4e9467b6323f03390c1c0dcefb8d252852df5
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-03-03 03:17:59 +0900

    Update editer.js

--- editer.js
+++ editer.js
@@ -1,6 +1,8 @@
 (function(root){
   /*v0.1 radio.cheked trans the 0 1
-    v0.2 style digital*/
+    v0.2 style digital
+    v0.3 kansuuji syou
+    */
 let fn={},is={},sys=root.sys||{},thenload=root.thenload;
 is.url=(d)=>{return /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i.test(d)}
 ;
@@ -32,7 +34,7 @@ fn.i=function(html,f,doc=document){
   var me=el.childNodes[0];
   return _f(me);
 }
-fn.kansuji=(num,keta=2)=>{
+fn.kansuji=(num,keta=2)=>{/*v0.3*/
   let c ='零壱弐参四伍六七八玖'.split('')
   ,a =('00000000'+num).slice(-1*keta).split('')
   ;
@@ -196,6 +198,11 @@ overflow:hidden;left:0;top:0}
 
 .rbtn:hover {
     background-color: #006b9f!important;
+}
+
+ol {/*v0.3 kansuuji*/
+    list-style: none;
+    padding-left: 1.5rem;
 }
 `;
   let layout =`
