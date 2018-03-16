95e8c4d230b9444e4b72d7eca57aa953e1c89210
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-03-16 15:00:06 +0900

    Update e2.js

--- e2.js
+++ e2.js
@@ -3,18 +3,20 @@
   is.element = function(obj){return !!(obj && obj.nodeType === 1)}
   ;
   fn.i3=(d)=>{
-    if(typeof d !=='string') return d
-    var el=document.createElement('table'); el.innerHTML=d.trim();
-    return el.childNodes[0];
+   if(typeof d !=='string') return d
+   var el=document.createElement('table'); el.innerHTML=d.trim();
+   return el.childNodes[0];
   }
   fn.effect=(el,o)=>{
-      let p =(d)=>{return isNaN( parseFloat(d))? 0 : parseFloat(d)*1000 }
-      Object.keys(o).forEach((k)=>{el.classList.add(k); 
-      setTimeout(()=>{ el.classList.remove(k) }, p(o[k]) )   
-     })
+   let p =(d)=>{return isNaN( parseFloat(d))? 0 : parseFloat(d)*1000 }
+   Object.keys(o).forEach((k)=>{el.classList.add(k); 
+   setTimeout(()=>{ el.classList.remove(k) }, p(o[k]) )   
+   })
   }
-  fn.r=(d=>d.parentNode.removeChild(d))
- ;   
+   fn.r=(d=>d.parentNode.removeChild(d))
+   fn.q=(s,doc=document)=>{return doc.querySelector(s)};
+   fn.qa=(s,doc=document)=>{return [].slice.call(doc.querySelectorAll(s))}
+  ;   
  function entry(obj){
     var e={};
     e.is =is; e.fn=fn
@@ -25,6 +27,9 @@
     e.psTo =(p)=>{p.parentNode.insertBefore(e.el,p);return e.el}
     e.effect =(o)=>{return fn.effect(e.el,o)}
     e.remove=()=>{return fn.r(e.el)}
+    e.q=(s)=>{return fn.q(s,e.el)}
+    e.qa=(s)=>{return fn.qa(s,e.el)}
+    e.log=()=>{console.log(e)}
     /*same*/
     e.a2=e.aTo; e.p2=e.pTo; e.as2=e.asTo; e.psTo=e.ps2;
     e.ef=e.effect; e.r=e.remove;
