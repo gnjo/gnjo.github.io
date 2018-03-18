4c4f32752df5ca083f3150d8c06ee3a7748fc7d9
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-03-18 18:08:04 +0900

    Update use.js

--- use.js
+++ use.js
@@ -43,7 +43,7 @@ fn.urlcnk=(u)=>{
  let cep = /.+\?/.test(u)? '&' :'?',v =`__${performance.now()}__=`.replace('.','')
  return u + cep + v + Date.now()
 }
-
+fn.timeToOrder=(time)=>{return 2147483647 - parseInt( time/1000 )}
 fn.hashCode =(s)=>{var h=0;for(var i=0;i < s.length; i++) h = h * 31 + s.charCodeAt(i)|0;return h}
 fn.mic12 =(s)=>{var d= fn.hashCode('GGGGGG'+s),a =d.toString(16).slice(-6);return a+a}
 fn.gistdesc =(u)=>{return new Date().toISOString().slice(0,"YYYY-MM".length) +'-'+fn.mic12(u)}
