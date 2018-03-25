137d4e6b13440536c81f64c3781aba30441ed248
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-03-25 21:49:31 +0900

    Update palette.js

--- palette.js
+++ palette.js
@@ -1,49 +1,79 @@
 
 ;(function(root){
  let fn={};
-fn.i3=function(html){
- if(typeof html !=='string') return html
- var el=document.createElement('table'); el.innerHTML=html.trim();
- return el.childNodes[0];
-}
-fn.copy=function(textVal){
- var copyFrom = document.createElement("textarea");
- copyFrom.textContent = textVal;
- var bodyElm = document.getElementsByTagName("body")[0];
- bodyElm.appendChild(copyFrom);
- copyFrom.select();
- var retVal = document.execCommand('copy');
- bodyElm.removeChild(copyFrom);
- return retVal;
-}
-fn.q=(s,doc=document)=>{return doc.querySelector(s)};
-fn.qa=(s,doc=document)=>{return [].slice.call(doc.querySelectorAll(s))}
-fn.ce=(d=>document.createElement(d))
-fn.r=(d=>d.parentNode.removeChild(d)) 
- ;function entry(str='#0a2133,#d1dec6,#961241,#6d8b89,#586c6c'){
+ fn.i3=function(html){
+  if(typeof html !=='string') return html
+  var el=document.createElement('table'); el.innerHTML=html.trim();
+  return el.childNodes[0];
+ }
+ fn.copy=function(textVal){
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = textVal;
+  var bodyElm = document.getElementsByTagName("body")[0];
+  bodyElm.appendChild(copyFrom);
+  copyFrom.select();
+  var retVal = document.execCommand('copy');
+  bodyElm.removeChild(copyFrom);
+  return retVal;
+ }
+ fn.q=(s,doc=document)=>{return doc.querySelector(s)};
+ fn.qa=(s,doc=document)=>{return [].slice.call(doc.querySelectorAll(s))}
+ fn.ce=(d=>document.createElement(d))
+ fn.r=(d=>d.parentNode.removeChild(d)) 
+ fn.sq=(d,opt=2)=>{
+ let f=(d)=>{return (d)?[
+   d.tagName.toLowerCase()
+   ,(d.classList.length!=0)?'.'+[].slice.call(d.classList).join('.'):''
+   ,(d.id&&d.id!='')?('#'+d.id):''
+   ,(d.name)?`[name="${d.name}"]`:''
+  ].join(''):null;
+ }
+ ,now=d
+ ;return Array.from({length:opt})
+  .map((d,i)=>{now= (now)?(i===0)?now:now.parentElement:null;return now})
+  .map(d=>f(d)).filter(d=>d).reverse().join('>')
+ ;
+} 
+;function entry(str='#0a2133,#d1dec6,#961241,#6d8b89,#586c6c'){
   let a=str.split(',');
   let layout=`
 <div data-palette>
- <style>
+<style>
 [data-palette]{position:fixed;bottom:0;right:25px;font-size:0}
-[data-palette]>b{font-family:monospace;font-size:10px;font-weight:400;padding:0 4px;}
-[data-palette]>b{width:50px;height:50px;display:inline-block;text-align:center;color:white}
-[data-palette]>b:hover{color:black;cursor:pointer}
- </style>
- <b style="background:${a[0]}">${a[0]}</b>
- <b style="background:${a[1]}">${a[1]}</b>
- <b style="background:${a[2]}">${a[2]}</b>
- <b style="background:${a[3]}">${a[3]}</b>
- <b style="background:${a[4]}">${a[4]}</b>
+[data-palette] b{font-family:monospace;font-size:10px;font-weight:400;}
+[data-palette] b{width:50px;height:50px;display:inline-block;text-align:center;color:white}
+[data-palette] b:hover{color:black;cursor:pointer}
+[data-palette] input{width:250px;box-sizing:border-box;outline:none;background:transparent;color:inherit;border:none;}
+[data-palette]>div{display: flex}
+</style>
+<input>xxxx</input>
+<div>
+<b style="background:${a[0]}">${a[0]}</b>
+<b style="background:${a[1]}">${a[1]}</b>
+<b style="background:${a[2]}">${a[2]}</b>
+<b style="background:${a[3]}">${a[3]}</b>
+<b style="background:${a[4]}">${a[4]}</b>
+</div>
 </div>
 `;
   if(fn.q('[data-palette]')) fn.r(fn.q('[data-palette]'));
   let x=fn.i3(layout);
   fn.qa('b',x).forEach(d=>{ d.onclick=()=>{ fn.copy(d.textContent) }})
-  document.body.appendChild(x);  
+  document.body.appendChild(x);
+  query();
  };
  root.palette =entry;
  root.palett=entry;
-//palette('#2b2515,#c6be62,#877e3f,#9fa04c,#6c5c64,https://i.imgur.com/zgXwPWM.jpg')
-})(this);
+ //palette('#2b2515,#c6be62,#877e3f,#9fa04c,#6c5c64,https://i.imgur.com/zgXwPWM.jpg');
 
+ function query(){
+  //ctrl clickで element のqueryをコピーする。
+  let el=fn.q('[data-palette] input')
+  el.onclick=()=>{ fn.copy(el.value)}
+  document.body.addEventListener('click',function(ev){
+   if(ev.target==el) return false;
+   ;el.value =fn.sq(ev.target,3);
+  });
+ }
+
+})(this);
