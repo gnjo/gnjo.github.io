791aa57251405dc349f572ecc923e0fb3bd69f04
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-03-28 23:03:56 +0900

    Update qgist.js

--- qgist.js
+++ qgist.js
@@ -2,6 +2,7 @@
  /*v0.1 _update()
   v0.2 null read
   v1.0 launched!
+  v1.1 delete 
  */
  var fn={},is={}
  is.url=(d)=>{return /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i.test(d)}
@@ -169,9 +170,26 @@
     o.data=d
     o.caller({url:url,opt:opt,type:'write',ret:o.data})
     return o.data;
-  })
-      
+  })     
  }
+ o.delete=(file)=>{
+  if(!o.data.files[file]) return Promise.resolve(null)
+  let filename=o.is.url(file)? o.fn.gistinfo(file).filename :file
+  //,content=data
+  ,body =JSON.stringify({
+   files:{
+   [filename]:null
+  }
+ })
+ ,url=o.targeturl
+ ,opt={method:'PATCH',mode:'cors',headers:{"Authorization":o.authstr},body:body}
+ ;
+  return fetch(url,opt).then(d=>d.json()).then(d=>{
+    o.data=d
+    o.caller({url:url,opt:opt,type:'delete',ret:o.data})
+    return o.data;
+  })     
+ } 
  ;
  root.qgist=o;
 })(this)
