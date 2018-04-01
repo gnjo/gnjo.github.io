5054bc28d027a05d03bc61c66a0c1b38e9426ebd
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-04-01 20:33:48 +0900

    Update e3.js

--- e3.js
+++ e3.js
@@ -1,3 +1,137 @@
+ ;(function(root){
+ /*v0.1 _update()
+  v0.2 null read
+  v1.0 launched!
+  v1.1 delete 
+  v1.2 data user define null
+  v1.3 treat 
+ */
+ var fn=root.fn||{},is=root.is||{};
+ is.jsonString =function(d){ try{JSON.parse(d);return true}catch(e){return false} } 
+ ;
+ fn.hashCode =(s)=>{var h=0;for(var i=0;i < s.length; i++) h = h * 31 + s.charCodeAt(i)|0;return h}
+ fn.mic12 =(s)=>{var d= fn.hashCode('GGGGGG'+s),a =d.toString(16).slice(-6);return a+a}
+ fn.jpTime=(timestamp=Date.now())=>{
+  return new Date(timestamp+1000*60*60*9)
+   .toISOString().replace(/-/g,'/').replace('T',' ').slice(0,'YYYY/MM/DD hh:mm'.length)
+ }
+ fn.gistinfo=(r)=>{
+  let ma =/https:\/\/gist.githubusercontent.com\/(.+)\/(.+)\/raw\/(.+)\/(.+)$/,a=r.match(ma)
+  return {url:r,user:a[1],id:a[2],filename:a[4]}  
+ }
+ ;
+ var o={
+  sysname:'___qgist___',public:false
+  ,user:null,data:null,micdesc:null,authstr:null,desc:null,targeturl:null
+  ,fn:fn,is:is
+ }
+ ;
+ o._update=()=>{
+  if(!o.targeturl) return console.log('not targeturl')
+  ;
+  let url =o.targeturl
+  ,opt={method:'GET',mode:'cors',headers:{"Authorization":o.authstr}}
+  ,calc=(d)=>{o.data=d;return o.data}
+  ;
+  return fetch(url,opt).then(d=>d.json()).then(calc)
+ }
+ //creation helper
+ o._createhelper=()=>{
+  return JSON.stringify({
+   public: o.publicc
+   ,description: `${o.micdesc}  ${o.desc}`
+   ,files:{[o.sysname]:{content: o.fn.jpTime()}}
+  })
+ }
+ o._create=()=>{
+  let body =o._createhelper()
+  ,url="https://api.github.com/gists"
+  ,opt={method:'POST',mode:'cors',headers:{"Authorization":o.authstr},body:body}
+  ,calc=(d)=>{o.data=d;o.targeturl=d.url;return o.data}
+  ;
+  return fetch(url,opt).then(d=>d.json()).then(calc)
+ }
+o._searchall=(max=20)=>{return new Promise(sol=>{
+  let ary=[],i=1
+  ,str =o.authstr
+  ,url =`https://api.github.com/users/${o.user.login}/gists`
+  ,opt={method:'GET',mode:'cors',headers:{"Authorization":str}}
+  ,f=()=>{
+   let u=`${url}?page=${i}`
+   ,loop =(d)=>{
+      if(d.length==0) return sol(ary)
+      ;[].push.apply(ary,d);
+      if(i>max) return sol(ary)
+      i++;
+      return f()
+   }
+   ;
+   return fetch(u,opt).then(res=>res.json()).then(loop)
+ }
+ ;
+ f()///
+ ;
+ })} 
+ o._user=function(){
+  let url='https://api.github.com/user'
+  ,opt={headers:{'authorization':o.authstr}}
+  ,calc=(d)=>{o.user=d; return d}
+  ;
+  return fetch(url,opt).then(d=>d.json()).then(calc)
+ }
+ o._auth=function(){
+  let url='https://api.github.com/gists/public'
+  ,opt={method:"HEAD",mode:"cors",headers:{"Authorization":o.authstr}}
+  ,check=(d=>d.status!=401&&d.ok)
+  ,_check=(d=>d.headers.get('X-RateLimit-Limit')!=60)
+  ;
+  return fetch(url,opt).then(_check).then(o._user)
+  ;
+ }
+ ;/*common*/
+ o.set=async (desc,authstr)=>{
+  o.desc=desc;
+  o.micdesc=o.fn.mic12(desc);
+  o.authstr=authstr;
+  if(!o.desc || !o.authstr){console.log('desc or authstr not');return}
+  ;
+  let d=await o._auth().then(o._searchall)
+  ,a=d.filter(d=>~d.description.indexOf(o.micdesc))
+  if(a.length==0) return o._create();///
+  o.data=a[0];
+  o.targeturl=a[0].url;
+  return o._update()///
+ } 
+ o.read=(file)=>{
+  //file or url
+  let url= (o.data.files[file])?o.data.files[file].raw_url:null
+  if(!url) return Promise.resolve(null) //bug fix;
+  return fetch(url).then(d=>d.text())
+ }
+ o.write=(file,data)=>{
+  let filename=file
+  ,content=data||''
+  ,body =JSON.stringify({ files:{ [filename]:{content: content}} })
+  ,url=o.targeturl
+  ,opt={method:'PATCH',mode:'cors',headers:{"Authorization":o.authstr},body:body}
+  ,calc=(d)=>{o.data=d;return o.data}
+  ;
+  return fetch(url,opt).then(d=>d.json()).then(calc)     
+ }
+ o.delete=(file)=>{
+  if(!o.data.files[file]) return Promise.resolve(null)
+  let filename=file
+  ,body =JSON.stringify({ files:{[filename]:null} })
+  ,url=o.targeturl
+  ,opt={method:'PATCH',mode:'cors',headers:{"Authorization":o.authstr},body:body}
+  ,calc=(d)=>{o.data=d;return o.data}
+  ;
+  return fetch(url,opt).then(d=>d.json()).then(calc)     
+ } 
+ ;
+ root.qgist=o;
+})(this); 
+
 ;(function(root){
 var is=root.is||{};
 is.jsonString =function(d){ try{JSON.parse(d);return true}catch(e){return false} } 
@@ -115,8 +249,6 @@ root.qg=qg;
   },false);
  }
 })(this);
-
-
 
 /*
 document.body.setI('aaa','bbbb');
