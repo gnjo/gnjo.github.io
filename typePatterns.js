3136d087135098aebcc6e94d0b6f39b9e1225cd3
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-04-17 21:21:20 +0900

    Update typePatterns.js

--- typePatterns.js
+++ typePatterns.js
@@ -1,15 +1,16 @@
+//v1.1 base ture or not. base count 16
 //[...,{n,v,s}]
 var typePatterns=[
- {n:'Infinity',v:Infinity}
- ,{n:'NaN',v:NaN}
- ,{n:'undefined',v:undefined,s:'"undefined"'}
- ,{n:'null',v:null,s:'"null"'}
- ,{n:'Object',v:{}}
- ,{n:'Function',v:new Function(';return;')}
- ,{n:'Boolean',v:new Boolean(true)}
+ {n:'Infinity',v:Infinity,b:true}
+ ,{n:'NaN',v:NaN,b:true}
+ ,{n:'undefined',v:undefined,s:'"undefined"',b:true}
+ ,{n:'null',v:null,s:'"null"',b:true}
+ ,{n:'Object',v:{},b:true}
+ ,{n:'Function',v:new Function(';return;'),b:true}
+ ,{n:'Boolean',v:new Boolean(true),b:true}
  /*,'Symbol':Symbol('Object')*/
  ,{n:'Error',v:new Error('dummy')}
- ,{n:'Number',v:new Number(1)}
+ ,{n:'Number',v:new Number(1),b:true}
  ,{n:'MathE',v:Math.E}
  ,{n:'MathLN2',v:Math.LN2}
  ,{n:'MathLN10',v:Math.LN10}
@@ -19,13 +20,13 @@ var typePatterns=[
  ,{n:'MathSQRT1_2',v:Math.SQRT1_2}
  ,{n:'MathSQRT2',v:Math.SQRT2}
  ,{n:'MathRandom',v:Math.random()}
- ,{n:'Date',v:new Date()}
+ ,{n:'Date',v:new Date(),b:true}
  ,{n:'DateNow',v:Date.now()}
  ,{n:'DateISO',v:new Date().toISOString()}
- ,{n:'String',v:new String('dummy')}
+ ,{n:'String',v:new String('dummy'),b:true}
  ,{n:'RegExp',v:new RegExp('.+')}
- ,{n:'Array',v:new Array(),s:'"[]"'}
- ,{n:'ArrayLike',v:{length:0},s:'"{length:0}"'}
+ ,{n:'Array',v:new Array(),s:'"[]"',b:true}
+ ,{n:'ArrayLike',v:{length:0},s:'"{length:0}"',b:true}
  ,{n:'ArrayEmpty',v:[],s:'"[]"'}
  ,{n:'ArrayInNumber',v:[0,1,2,3]}
  ,{n:'ArrayInString',v:['a','b','c','d'] }
@@ -35,14 +36,14 @@ var typePatterns=[
  ,{n:'Set',v:new Set()}
  ,{n:'ArrayBuffer',v:new ArrayBuffer()}
  ,{n:'Proxy',v:new Proxy({},{})}
- ,{n:'Promise',v:new Promise(()=>{})}
+ ,{n:'Promise',v:new Promise(()=>{}),b:true}
  //promise reject is always born the error... so cut.
  //,{n:'PromiseResolve',v:Promise.resolve('dummy'),s:'Promise.resolve("dummy")'}
  //,{n:'PromiseReject',v:Promise.reject('dummy'),s:'Promise.reject("dummy")'}
  ,{n:'JSONString',v:JSON.stringify({ary:[0,1,2,3],x:{v:'dummy'}})}
  /*addition*/
- ,{n:'False',v:false}
- ,{n:'True',v:true}
+ ,{n:'False',v:false,b:true}
+ ,{n:'True',v:true,b:true}
  ,{n:'Number0',v:0}
  ,{n:'Number1',v:1}
  ,{n:'Number-1',v:-1}
@@ -51,7 +52,7 @@ var typePatterns=[
  ,{n:'NumberMAX_VALUE',v:Number.MAX_VALUE}
  ,{n:'NumberMIN_VALUE',v:Number.MIN_VALUE}
  ,{n:'NumberNaN',v:Number.NaN}
- ,{n:'StringEmpty',v:'',s:'""'}
+ ,{n:'StringEmpty',v:'',s:'""',b:true}
  ,{n:'String0',v:'0'}
  ,{n:'String1',v:'1'}
  ,{n:'String-1',v:'-1'} 
