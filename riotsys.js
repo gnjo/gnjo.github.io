4703ee88ec37fc6485efb776c569cfccb3392e45
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-04-21 15:53:13 +0900

    Update riotsys.js

--- riotsys.js
+++ riotsys.js
@@ -70,7 +70,7 @@
  sys.load=()=>{ sys.data=ls.getI(sys.key)||{}; sys.trigger('loaded') }
  sys.on('load',function(){ sys.load()})
  sys.on('save',function(){ sys.save()})
- sys.on('savede',function(){ console.log('in savede');if(sys.autosave) sys.savede() })
+ sys.on('savede',function(){ /*console.log('in savede');*/ if(sys.autosave) sys.savede() })
  let cs=[],share=()=>{ cs.filter(d=>d).map(d=>d.trigger('changed',sys.data))}
  ,init =(d)=>{d.on('change',(o)=>{ Object.assign(sys.data,o);/**/sys.trigger('savede');/**/ share() })}
  ;
