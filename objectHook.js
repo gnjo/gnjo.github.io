d75d1f2f44ed7887c6d38304db9d2648e2cf8424
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-04-13 16:31:04 +0900

    Create objectHook.js

---
+++ objectHook.js
@@ -0,0 +1,35 @@
+;(function(root){
+ 'use strict'
+ ;
+ function entry(obj,caller){
+  var is={}
+  is.function = function(obj){return toString.call(obj) === '[object Function]'}
+  ;
+  var o =obj,keys=Object.keys(o.__proto__).concat(Object.keys(o)).filter(d=>is.function(o[d]))
+  console.log('hook object=>',o);console.log('hook funcitons=>',keys)
+  ;
+  keys.map(key=>{
+   var dump= o[key]
+   o[key]=function(){
+    var args=Array.from(arguments)
+    caller.apply(o,[key].concat(args))
+    return dump.apply(o,args)
+   }
+  })
+ }
+ ;
+ root.objectHook=entry
+ ;
+ /*usage
+var obj=localStorage//console
+objectHook(obj,function(){
+ var type=arguments[0]
+ var args=Array.from(arguments).slice(1)
+ let x=document.createElement('pre');
+ x.textContent=type+':'+JSON.stringify(args,null,'\t');
+ document.body.appendChild(x)
+ //
+})
+localStorage.setItem('xyz','bbbb')
+ */
+})(this);
