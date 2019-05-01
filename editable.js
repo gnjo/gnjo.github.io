c9e0359c6ef963c7b8466c3ad3a13397d8bb2f27
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-05-01 13:31:26 +0900

    wrote element bugfix

--- editable.js
+++ editable.js
@@ -1,3 +1,8 @@
+/*history
+v1.0 create
+v1.1 data-length map
+v1.2 bugfix add remove wrote element
+*/
 ;(function(root){
  'use strict';
  ;
@@ -6,9 +11,6 @@
   let target =_target.replace(/\./g,'').split(',')
   ,flg=_flg
   ,body=document.body
-  ,remove=function(e){
-   e.target.removeAttribute('contenteditable')
-  }
   ,hasClass=function(el){
    let l=target.filter(d=>el.classList.contains(d)).length
    return (l>0)?true:false;
@@ -16,13 +18,19 @@
   ,lmap=function(e){
    e.target.dataset.length=e.target.textContent.length
   }
+  ,remove=function(e){
+   let el=e.target
+   el.removeAttribute('contenteditable')
+   if(flg)el.removeEventListener('input',lmap)
+   el.dataset.editable=false
+  }  
   ,add=function(e){
    if(!hasClass(e.target))return
    let el=e.target
    el.setAttribute('contenteditable','plaintext-only')
    el.focus()
-   if(el.dataset.editable)return
-   el.addEventListener('blur',remove)
+   //if(el.dataset.editable)return
+   el.addEventListener('blur',remove,{once:true})
    if(flg)el.addEventListener('input',lmap)
    el.dataset.editable=true
    el=void 0
