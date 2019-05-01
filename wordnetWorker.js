1b969cc1873889a628f47179b1be4b8e294ee386
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-05-01 17:19:04 +0900

    Create wordnetWorker.js

---
+++ wordnetWorker.js
@@ -0,0 +1,25 @@
+ /*usage
+let sel=fn.q('.l'),ret=fn.q('.r'),w=fn.worker(fn.q('#edict'))
+;
+sel.onselect=function(ev){ w.postMessage(fn.gsl()) }
+w.onmessage=function(ev){ ret.textContent = ev.data.map(d=>d[2]).join('/') } 
+ */
+var ary=void 0
+,url='https://gnjo.github.io/wnjpn-ok.tab.json'
+fetch(url).then(d=>d.json()).then(d=>{ary=d})
+;
+ 
+function calc(word='',max=5){
+ if(word.length===0) return []
+ try{
+ return ary.filter(d=> ~d[2].indexOf(word) ).slice(0,max) //search the containts words
+  .map(d=>d[0]).map(num=> ary.filter(d=>d[0]===num) ) //search syno...m
+  .reduce((a,b)=> a.concat(b),[])  //flattan
+ }catch(e){return []}
+}
+;
+onmessage=function(e){ 
+ if(!ary) return;
+ postMessage(calc(e.data))
+}
+;
