3aa159a307c1f6cbf9bac5e8d19b3125370b7a83
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-05-05 20:59:13 +0900

    Update use.js

--- use.js
+++ use.js
@@ -672,3 +672,26 @@ fn.interval(2000,(id)=>{
   }
   fn.gp=fn.getParam=fn.getparam;
 
+fn.crcTable=(function(){
+  var c,crcTable = [];
+  for(var n =0; n < 256; n++){
+    c = n;
+    for(var k =0; k < 8; k++){
+      c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+    }
+    crcTable[n] = c;
+  }
+  return crcTable;
+})();//early gen
+fn.crc32 = function(str,hex=true) {
+  var crcTable = fn.crcTable
+  var pad=( (d,l)=>('000000000000000000'+d).slice(-1*l));
+  var crc = 0 ^ (-1);
+  for (var i = 0; i < str.length; i++ ) {
+    crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+  }
+  crc = (crc ^ (-1)) >>> 0;
+  if(hex) return pad(crc.toString(16),8)
+  else return crc
+};
+
