de72ecbb619a2899bcc76a59a2f769f52f342583
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-05-28 19:38:47 +0900

    Update use.js

--- use.js
+++ use.js
@@ -777,3 +777,36 @@ fn.worker=(src)=>{
   return new Worker( is.element(src)?f(src.textContent):is.url(src)?src:f(src) );
  }
 
+fn.getCaret=function getCaretPosition(editableDiv) {
+ var caretPos = 0,
+     sel, range;
+ if (window.getSelection) {
+  sel = window.getSelection();
+  if (sel.rangeCount) {
+   range = sel.getRangeAt(0);
+   if (range.commonAncestorContainer.parentNode == editableDiv) {
+    caretPos = range.endOffset;
+   }
+  }
+ } else if (document.selection && document.selection.createRange) {
+  range = document.selection.createRange();
+  if (range.parentElement() == editableDiv) {
+   var tempEl = document.createElement("span");
+   editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+   var tempRange = range.duplicate();
+   tempRange.moveToElementText(tempEl);
+   tempRange.setEndPoint("EndToEnd", range);
+   caretPos = tempRange.text.length;
+  }
+ }
+ return caretPos;
+}
+fn.setCaret=function setCaretPosition(editableDiv,pos){
+ var range = document.createRange();
+ var sel = window.getSelection();
+ range.setStart(editableDiv.firstChild,pos);
+ range.collapse(true);
+ sel.removeAllRanges();
+ sel.addRange(range);
+}
+
