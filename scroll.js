9caf1f274865b479a3af28c12d6722a1834ef9fa
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-09-02 18:34:08 +0900

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,42 @@
+;(function(root){
+ //console.log('in')
+ if(!document.body.dataset.scroll){
+  let el=document.createElement('style')
+  el.classList.add('scroll')
+  document.head.appendChild(el)
+  document.body.dataset.scroll='true';  
+ }
+ //console.log('a')
+
+ let css=`
+::-webkit-scrollbar-thumb {
+  background: %%c%%;
+  border-radius: 0px;
+}
+
+.hidescrollbar::-webkit-scrollbar
+,.hidescroll::-webkit-scrollbar {
+  width: 0px;
+}
+
+::-webkit-scrollbar {
+  background-color: %%bg%%;
+  width: 8px;
+}
+
+::-webkit-scrollbar-button {
+  background-color: %%bg%%;
+  height: 0px;
+  width: 0px;
+  border-radius: 0px;
+}
+`
+ 
+ function entry(bg,c){
+  let el=document.querySelector('style.scroll')
+  el.textContent=css.replace(/%%bg%%/g,bg).replace(/%%c%%/g,c)
+ }
+ root.scroll=entry
+})(this);
+
+//scroll('#000','#aaa')
