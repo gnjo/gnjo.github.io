2a31ca62c3d374973090c5b2e8703e1463bd760e
gunjyo <35440828+gnjo@users.noreply.github.com>
2019-09-06 12:39:10 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -10,38 +10,48 @@
 
  let css=`
 body{
-background:%%bg%%;
-color:%%c%%;
+background:%%c1%%;
+color:%%c2%%;
 }
 
 ::-webkit-scrollbar-thumb {
-  background: %%c%%;
-  border-radius: 0px;
+background: %%c2%%;
+border-radius: 0px;
 }
 
 .hidescrollbar::-webkit-scrollbar
 ,.hidescroll::-webkit-scrollbar {
-  width: 0px;
+width: 0px;
 }
 
 ::-webkit-scrollbar {
-  background-color: %%bg%%;
-  width: 8px;
+background-color: %%c1%%;
+width: 8px;
 }
 
 ::-webkit-scrollbar-button {
-  background-color: %%bg%%;
-  height: 0px;
-  width: 0px;
-  border-radius: 0px;
+background-color: %%c1%%;
+height: 0px;
+width: 0px;
+border-radius: 0px;
 }
+.bg1{background-color:%%c1%%;}
+.b1{border:2px solid %%c1%%;}
+.c1{color:%%c1%%;}
+
+.bg2{background-color:%%c2%%;}
+.b2{border:2px solid %%c2%%;}
+.c2{color:%%c2%%;}
+
+.bg3{ background-color:%%c3%%;}
+.b3{border:2px solid %%c3%%;}
+.c3{color:%%c3%%;}
 `
- 
- function entry(bg,c){
-  let el=document.querySelector('style.scroll')
-  el.innerHTML=css.replace(/%%bg%%/g,bg).replace(/%%c%%/g,c)
+
+ function entry(c1,c2,c3){
+  let el=document.querySelector('style.scroll')  el.innerHTML=css.replace(/%%c1%%/g,c1).replace(/%%c2%%/g,c2).replace(/%%c3%%/g,c3)
  }
  root.scroll=entry
 })(this);
-
-//scroll('#000','#aaa')
+//.bg1,.c1,.b1,.bg2 ...
+//scroll('#000','#aaa','#000')
