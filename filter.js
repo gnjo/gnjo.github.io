aa41bdbae19bb99247473aff447b19658a0a5823
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-03-03 13:51:09 +0900

    Update filter.js

--- filter.js
+++ filter.js
@@ -368,44 +368,6 @@ function _sobel(data,w,h) {
 }
   
 function _dither2(data,w,h){
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
-} 
-const bayer = [
- shuffle([0*16+8, 8*16+8, 2*16+8, 10*16+8]),
- shuffle([12*16+8, 4*16+8, 14*16+8, 6*16+8]),
- shuffle([3*16+8, 11*16+8, 1*16+8, 9*16+8]),
- shuffle([15*16+8, 7*16+8, 13*16+8, 5*16+8])
-]; 
-  
-/*const bayer = [
- [0*16+8, 8*16+8, 2*16+8, 10*16+8],
- [12*16+8, 4*16+8, 14*16+8, 6*16+8],
- [3*16+8, 11*16+8, 1*16+8, 9*16+8],
- [15*16+8, 7*16+8, 13*16+8, 5*16+8]
-]; */
- let x,y,chk,wk
- for (let i = 0; i < data.length; i += 4) {
-  wk=~~(i/4)
-  y=~~(wk/w)
-  x=~~(wk%w)
-  //console.log(wk,x,y,w)
-  chk=bayer[y%4][x%4]
-  data[i]   = (data[i] >= chk)?0xff:0x00
-  data[i+1] = (data[i+1] >= chk)?0xff:0x00
-  data[i+2] = (data[i+2] >= chk)?0xff:0x00
- }
- return data;
-}
-
-function _dither3(data,w,h){
   let lrot = a => a[0].map((_, c) => a.map(r => r[c]).reverse());   
 const bayer =lrot( [
  [0*16+8, 8*16+8, 2*16+8, 10*16+8],
@@ -426,9 +388,52 @@ const bayer =lrot( [
   data[i+2] = (data[i+2] >= chk)?0xff:0x00
  }
  return data;
-}
+}  
+function _dither3(data,w,h){
+  let lrot = a => a[0].map((_, c) => a.map(r => r[c]).reverse());   
+const bayer =lrot(lrot( [
+ [0*16+8, 8*16+8, 2*16+8, 10*16+8],
+ [12*16+8, 4*16+8, 14*16+8, 6*16+8],
+ [3*16+8, 11*16+8, 1*16+8, 9*16+8],
+ [15*16+8, 7*16+8, 13*16+8, 5*16+8]
+] ))
+;
+ let x,y,chk,wk
+ for (let i = 0; i < data.length; i += 4) {
+  wk=~~(i/4)
+  y=~~(wk/w)
+  x=~~(wk%w)
+  //console.log(wk,x,y,w)
+  chk=bayer[y%4][x%4]
+  data[i]   = (data[i] >= chk)?0xff:0x00
+  data[i+1] = (data[i+1] >= chk)?0xff:0x00
+  data[i+2] = (data[i+2] >= chk)?0xff:0x00
+ }
+ return data;
+}  
 
-  
+function _dither4(data,w,h){
+  let lrot = a => a[0].map((_, c) => a.map(r => r[c]).reverse());   
+const bayer =lrot(lrot(lrot( [
+ [0*16+8, 8*16+8, 2*16+8, 10*16+8],
+ [12*16+8, 4*16+8, 14*16+8, 6*16+8],
+ [3*16+8, 11*16+8, 1*16+8, 9*16+8],
+ [15*16+8, 7*16+8, 13*16+8, 5*16+8]
+] )))
+;
+ let x,y,chk,wk
+ for (let i = 0; i < data.length; i += 4) {
+  wk=~~(i/4)
+  y=~~(wk/w)
+  x=~~(wk%w)
+  //console.log(wk,x,y,w)
+  chk=bayer[y%4][x%4]
+  data[i]   = (data[i] >= chk)?0xff:0x00
+  data[i+1] = (data[i+1] >= chk)?0xff:0x00
+  data[i+2] = (data[i+2] >= chk)?0xff:0x00
+ }
+ return data;
+}  
   
 function _dither(data,w,h){
   
@@ -472,8 +477,10 @@ o._green=_green
 o._sobel=_sobel  
 //add
 o._dither=_dither  
-o._dither2=_dither2
+o._dither1=_dither    
+o._dither2=_dither2  
 o._dither3=_dither3
+o._dither4=_dither4
   
  root.filter =o;
  
