5fcdbf3f6add322b209ea55d80978f2b82db0706
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-03-04 19:16:01 +0900

    Update imageReader.js

--- imageReader.js
+++ imageReader.js
@@ -7,20 +7,20 @@
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
   }
-  function handleFileSelect(caller) {return function(evt){
+  function handleFileSelect(caller,max) {return function(evt){
     evt.stopPropagation();
     evt.preventDefault();
     //both ondrop and input type=file
     var files = evt.target.files||evt.dataTransfer.files;
     // Loop through the FileList and render image files as thumbnails.
-    readFile(files,caller);
+    readFile(files,caller,max);
   }}
-  function readFile(files,caller){
+  function readFile(files,caller,max){
     if(files.length==0) return;
-    if(files.length>5) console.log('max 5files ');
+    if(files.length>max) console.log('max '+max+'files ');
     ;[].slice.call(files)
       .filter(f=>f.type.match('image.*'))
-      .slice(0,5)
+      .slice(0,max)
       .map(file=>{
       var reader = new FileReader();
       reader.onload = (function (thefile,thecaller) {
@@ -32,12 +32,12 @@
       reader.readAsDataURL(file);
     })    
     }  
-  function imageReader(el,caller){
+  function imageReader(el,caller,max){
     var o={};
     o.el=el;
     o.caller=caller||function(ev){/**/};
     o.el.ondragover =handleDragOver;//
-    o.el.ondrop=handleFileSelect(o.caller);//
+    o.el.ondrop=handleFileSelect(o.caller,max||10);//
   }
   root.imageReader=imageReader;
  /*usage
